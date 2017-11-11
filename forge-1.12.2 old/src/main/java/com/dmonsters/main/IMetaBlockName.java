e47d43426b924d2bd2bf1f877d136814468b897a
bigbang87 <bigbang87@gmail.com>
2017-11-11 21:07:38 +0100

    some cleanup

--- forge-1.12.2 old/src/main/java/com/dmonsters/main/IMetaBlockName.java
+++
@@ -1,7 +0,0 @@
-package com.dmonsters.main;
-
-import net.minecraft.item.ItemStack;
-
-public interface IMetaBlockName {
-	String getSpecialName(ItemStack stack);
-}
--- forge-1.12.2 old/src/main/java/com/dmonsters/proxy/ServerProxy.java
+++
@@ -1,5 +0,0 @@
-package com.dmonsters.proxy;
-
-public class ServerProxy extends CommonProxy {
-
-}
